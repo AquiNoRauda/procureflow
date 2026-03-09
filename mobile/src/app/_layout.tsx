39c290218eeec39300b2ac29343a3b97b1ec9ec6
Sandbox user <tools@notanumber.com>
2026-03-09 23:24:55 +0000

    Bring the draft window just a bit down. I don’t want it to cover the whole screen

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -35,7 +35,7 @@ function RootLayoutNav({ colorScheme }: { colorScheme: 'light' | 'dark' | null |
           options={{
             presentation: 'formSheet',
             sheetGrabberVisible: true,
-            sheetAllowedDetents: [1.0],
+            sheetAllowedDetents: [0.75],
             headerShown: false,
           }}
         />
